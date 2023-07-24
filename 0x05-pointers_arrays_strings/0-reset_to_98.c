8c1728e55799e3f9841470c4d7a1dff51d188f92
Luis <howin349@gmail.com>
2023-07-24 13:37:36 +0300

    r

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void reset_to_98(int *n){
+	*n=98;
+}
+
+int main() {
+	int num = 5;
+	printf("Before calling reset_to_98: %d\n", num);
+
+    reset_to_98(&num);
+
+    printf("After calling reset_to_98: %d\n", num);
+
+    return 0;
+}	
