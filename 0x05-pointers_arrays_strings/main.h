d6e9215d0726339a7d66db199b807f77d422f5f8
Luis <howin349@gmail.com>
2023-07-25 00:20:49 +0300

    d

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *5);
-void _puts()char *str;
+void _puts(char *str);
 void print_rev(char *5);
 void rev_string(char *5);
 void puts2(char *str);
