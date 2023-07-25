8b49280af4c62d99b312d61b9e1220ff82d40799
Luis <howin349@gmail.com>
2023-07-25 14:02:18 +0300

    d

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,9 +2,8 @@
 
 /**
  * puts_half - a function that prints half of a string
- * if odd len, n = (length_of_the_string - 1) / 2
  * @str: input
- * Return: half of input
+ * Return: no return
  */
 
 void puts_half(char *str)
