36f2be3092a72a23026e25b38da5df16def2bd33
Luis <howin349@gmail.com>
2023-07-25 14:15:49 +0300

    d

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,8 @@
-#ifdef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *5);
+int _atoi(char *5);
 void _puts(char *str);
 void print_rev(char *5);
 void rev_string(char *5);
@@ -16,4 +11,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif
