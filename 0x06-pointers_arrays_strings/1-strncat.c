32d8e8bec6200ae370351db5f08b492245162924
Luis <howin349@gmail.com>
2023-07-28 03:18:26 +0300

    r

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _strncat - concatenates two strings,
  * @dest: destination.
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * reverse_array - reverses the content of an array
  * of integers.
