e6ed546cc764067dd3300e47edd685fe4da9fcae
Luis Howin <62960965+luishowin@users.noreply.github.com>
2023-10-11 09:14:25 +0300

    fwrf

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <function_pointers.h>
+
+/**
+ * print_name_as_is - prints name as is
+ * @name: name of the person
+ * 
+ * Return: Nothing
+*/
+void print_name(char *name, void (*f)(char *));
+{
+    if(name && f)
+    {
+        f(name);
+    }
+}
