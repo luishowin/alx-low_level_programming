8e3408244d3babab98f716bfe88c554d1101081b
Luis Howin <62960965+luishowin@users.noreply.github.com>
2023-11-19 23:23:50 +0300

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,39 @@
-#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
-#include "holberton.h"
-
+#include "main.h"
 /**
- * create_file - creates a file.
- * @filename: var ptr
- * @text_content: content file
- * Return: 1 on success, -1 on failure
- */
+ * create_file - creates a file 
+ * file_descriptor - // Open the file with read and write permissions, creating it if it doesn't exist
+ * 
+*/
 
-int create_file(const char *filename, char *text_content)
-{
-	int i = 0, file;
+int create_file(const char *filename, char *text_content) {
+    if (filename == NULL) {
+        return -1; /*if filename is NULL*/
+    }
 
-	if (filename == NULL)
-		return (-1);
+    int file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (text_content == NULL)
-		text_content = "";
+    if (file_descriptor == -1) {
+        return -1; /*if the file cannot be opened or created*/
+    }
 
+    if (text_content != NULL) {
+        // Write the content to the file if text_content is not NULL
+        ssize_t write_result = write(file_descriptor, text_content, strlen(text_content));
 
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
+        if (write_result == -1) {
+            close(file_descriptor);
+            return -1; // Return -1 if the write operation fails
+        }
+    }
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    // Close the file descriptor
+    if (close(file_descriptor) == -1) {
+        return -1; // Return -1 if the file cannot be closed
+    }
 
-	if (file == -1)
-		return (-1);
-
-	write(file, text_content, i);
-
-	return (1);
+    return 1;
 }
