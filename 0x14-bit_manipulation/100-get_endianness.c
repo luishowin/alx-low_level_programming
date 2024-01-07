b877a4bbc59d3827899a230ba31214d707857be8
Luis Howin <62960965+luishowin@users.noreply.github.com>
2024-01-07 15:46:43 +0300

    f

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+/**
+ * get_endianness - Checks the endianness of the system.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+    unsigned int num = 1;
+    char *byte_pointer = (char *)&num;
+
+    return (*byte_pointer == 1) ? 1 : 0;
+}
