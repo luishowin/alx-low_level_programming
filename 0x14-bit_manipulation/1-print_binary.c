3a7c362edb366bfdfe739574a4c091cc52e9e11b
Luis Howin <62960965+luishowin@users.noreply.github.com>
2024-01-07 15:34:25 +0300

    vf

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
+ */
+void print_binary(unsigned long int n)
+{
+    if (n > 1)
+        print_binary(n >> 1);
+
+    putchar((n & 1) + '0');
+}
+
+int main(void)
+{
+    unsigned long int num = 42;
+    printf("Binary representation of %lu: ", num);
+    print_binary(num);
+    putchar('\n');
+    return 0;
+}
